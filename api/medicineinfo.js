26de57e00a07d6fa386e51995d833c1f75a67c51
nishanth <prasannadasari006@gmail.com>
2026-06-22 15:46:56 +0530

    Test medicineinfo

--- api/medicineinfo.js
+++ api/medicineinfo.js
@@ -1,79 +1,8 @@
 export default async function handler(req, res) {
 
-  try {
-
-    const { medicineName } =
-      req.body;
-
-    const response =
-      await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type":
-              "application/json"
-          },
-          body: JSON.stringify({
-            contents: [
-              {
-                parts: [
-                  {
-                    text: `
-Medicine Name: ${medicineName}
-
-Reply ONLY in JSON format.
-
-{
-  "uses":"Short medicine uses in English",
-  "teluguExplanation":"Simple Telugu explanation in Telugu"
-}
-`
-                  }
-                ]
-              }
-            ]
-          })
-        }
-      );
-
-    const data =
-      await response.json();
-
-    console.log(
-      JSON.stringify(data, null, 2)
-    );
-
-    const text =
-      data?.candidates?.[0]
-        ?.content?.parts?.[0]
-        ?.text || "{}";
-
-    const cleaned =
-      text
-        .replace(/```json/g, "")
-        .replace(/```/g, "")
-        .trim();
-
-    return res
-      .status(200)
-      .json(
-        JSON.parse(cleaned)
-      );
-
-  }
-
-  catch (error) {
-
-    console.error(error);
-
-    return res.status(500).json({
-      uses:
-        "Information unavailable",
-      teluguExplanation:
-        "సమాచారం అందుబాటులో లేదు"
-    });
-
-  }
+  return res.status(200).json({
+    uses: "TEST USES",
+    teluguExplanation: "TEST TELUGU"
+  });
 
 }
